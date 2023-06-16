ac4d3cb8662b8c884356213a3d60fdb9412b6fbf
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2023-06-16 10:57:57 +0200

    overhaul + testing improved frontend for index.html

--- client/tabs.js
+++ client/tabs.js
@@ -1,24 +1,24 @@
 var ex_tab //existent tab
 {   
-    let dark = "#bcbbba"
-    let light = "#efeded"
+    let light = "#77e2d7"
+    let dark = "#1D6F75"
 
     let div = document.getElementById('tabs-buttons')
     let contDiv=document.getElementById('tabs-content')
     ex_tab=contDiv.children[0]
     ex_btn=div.children[0]
-    ex_btn.style.backgroundColor=dark
+    ex_btn.style.backgroundColor=light
 
     
 
     //switch tab
     let sTab = (ev)=>{
         ex_tab.hidden = true
-        ex_btn.style.backgroundColor=light
+        ex_btn.style.backgroundColor=dark
         ex_tab=contDiv.children[ev.target.value]
         ex_tab.hidden = false
         ex_btn=div.children[ev.target.value]
-        ex_btn.style.backgroundColor=dark
+        ex_btn.style.backgroundColor=light
         
     }
 
