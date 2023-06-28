fe57aaa5a0b3bf372a51f39e617014dfac5305f1
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2023-06-28 14:52:30 +0200

    splitted audio to a file + better notification div

--- client/room/app.js
+++ client/room/app.js
@@ -23,14 +23,6 @@ if (isIOS) {
     }
 }
 
-const AUDIO_BLOCK_SIZE = 1024
-const AUDIO_FIFO_MAXLEN = 4900
-var audioContext
-var scriptProcessor
-var audioFifo0 = new Int16Array(AUDIO_FIFO_MAXLEN)
-var audioFifo1 = new Int16Array(AUDIO_FIFO_MAXLEN)
-var audioFifoHead = 0
-var audioFifoCnt = 0
 
 var fileInput = document.getElementById('romFile')
 var canvas = document.getElementById('gba-canvas')
@@ -67,78 +59,8 @@ var showMsgStop
 const inputs = new L3GBAInputs()
 inputs.init()
 
-function processAudio(event) {
-    var outputBuffer = event.outputBuffer
-    var audioData0 = outputBuffer.getChannelData(0)
-    var audioData1 = outputBuffer.getChannelData(1)
-
-    if ((!isRunning) ) {
-        for (var i = 0; i < AUDIO_BLOCK_SIZE; i++) {
-            audioData0[i] = 0
-            audioData1[i] = 0
-        }
-        return
-    }
-    while (audioFifoCnt < AUDIO_BLOCK_SIZE) {
-        //console.log('audio fifo underflow, running a new frame')
-        emuRunFrame();
-    }
-
-    var copySize = AUDIO_BLOCK_SIZE
-    if (audioFifoCnt < copySize) {
-        copySize = audioFifoCnt
-    }
-    for (var i = 0; i < copySize; i++) {
-        audioData0[i] = audioFifo0[audioFifoHead] / 32768.0
-        audioData1[i] = audioFifo1[audioFifoHead] / 32768.0
-        audioFifoHead = (audioFifoHead + 1) % AUDIO_FIFO_MAXLEN
-        audioFifoCnt--
-    }
-}
-
-// must be called in user gesture
-function tryInitSound() {
-    if (audioContext) {
-        if (audioContext.state != 'running') {
-            audioContext.resume()
-        }
-        return;
-    }
-    try {
-        audioContext = new (window.AudioContext || window.webkitAudioContext)({ latencyHint: 0.0001, sampleRate: 48000 });
-        scriptProcessor = audioContext.createScriptProcessor(AUDIO_BLOCK_SIZE, 0, 2)
-        scriptProcessor.onaudioprocess = processAudio
-        scriptProcessor.connect(audioContext.destination)
-
-        audioContext.resume()
-    } catch (e) {
-        console.log(e)
-        //alert('Cannnot init sound ')
-    }
-}
 
 
-
-function writeAudio(ptr, frames) {
-    //console.log(ptr, frames)
-    /*if (turboMode) {
-        return
-    }*/
-    if (!wasmAudioBuf) {
-        wasmAudioBuf = new Int16Array(Module.HEAPU8.buffer).subarray(ptr / 2, ptr / 2 + 2048)
-    }
-    var tail = (audioFifoHead + audioFifoCnt) % AUDIO_FIFO_MAXLEN
-    if (audioFifoCnt + frames >= AUDIO_FIFO_MAXLEN) {
-        //console.log('o', audioFifoCnt)
-        return
-    }
-    for (var i = 0; i < frames; i++) {
-        audioFifo0[tail] = wasmAudioBuf[i * 2]
-        audioFifo1[tail] = wasmAudioBuf[i * 2 + 1]
-        tail = (tail + 1) % AUDIO_FIFO_MAXLEN
-    }
-    audioFifoCnt += frames
-}
 
 function wasmReady() {
     romBuffer = Module._emuGetSymbol(1)
@@ -408,13 +330,13 @@ function clearSaveBufState() {
 
 
 function showMsg(msg, time) {
+    clearTimeout(showMsgStop)
     let timeShown = time || 1000
     document.getElementById('notif-msg').innerText = msg
     document.getElementById('notif').hidden = false
     showMsgStop = setTimeout(function () {
         document.getElementById('notif').hidden = true
         //prevents messages from annoying each others.
-        clearTimeout(showMsgStop)
     }, timeShown)
 }
 
---
+++ client/room/audio.js
@@ -0,0 +1,82 @@
+const AUDIO_BLOCK_SIZE = 1024
+const AUDIO_FIFO_MAXLEN = 4900
+var audioContext
+var scriptProcessor
+var audioFifo0 = new Int16Array(AUDIO_FIFO_MAXLEN)
+var audioFifo1 = new Int16Array(AUDIO_FIFO_MAXLEN)
+var audioFifoHead = 0
+var audioFifoCnt = 0
+
+
+function processAudio(event) {
+    var outputBuffer = event.outputBuffer
+    var audioData0 = outputBuffer.getChannelData(0)
+    var audioData1 = outputBuffer.getChannelData(1)
+
+    if ((!isRunning) ) {
+        for (var i = 0; i < AUDIO_BLOCK_SIZE; i++) {
+            audioData0[i] = 0
+            audioData1[i] = 0
+        }
+        return
+    }
+    while (audioFifoCnt < AUDIO_BLOCK_SIZE) {
+        //console.log('audio fifo underflow, running a new frame')
+        emuRunFrame();
+    }
+
+    var copySize = AUDIO_BLOCK_SIZE
+    if (audioFifoCnt < copySize) {
+        copySize = audioFifoCnt
+    }
+    for (var i = 0; i < copySize; i++) {
+        audioData0[i] = audioFifo0[audioFifoHead] / 32768.0
+        audioData1[i] = audioFifo1[audioFifoHead] / 32768.0
+        audioFifoHead = (audioFifoHead + 1) % AUDIO_FIFO_MAXLEN
+        audioFifoCnt--
+    }
+}
+
+// must be called in user gesture
+function tryInitSound() {
+    if (audioContext) {
+        if (audioContext.state != 'running') {
+            audioContext.resume()
+        }
+        return;
+    }
+    try {
+        audioContext = new (window.AudioContext || window.webkitAudioContext)({ latencyHint: 0.0001, sampleRate: 48000 });
+        scriptProcessor = audioContext.createScriptProcessor(AUDIO_BLOCK_SIZE, 0, 2)
+        scriptProcessor.onaudioprocess = processAudio
+        scriptProcessor.connect(audioContext.destination)
+
+        audioContext.resume()
+    } catch (e) {
+        console.log(e)
+        //alert('Cannnot init sound ')
+    }
+}
+
+
+
+function writeAudio(ptr, frames) {
+    //console.log(ptr, frames)
+    /*if (turboMode) {
+        return
+    }*/
+    if (!wasmAudioBuf) {
+        wasmAudioBuf = new Int16Array(Module.HEAPU8.buffer).subarray(ptr / 2, ptr / 2 + 2048)
+    }
+    var tail = (audioFifoHead + audioFifoCnt) % AUDIO_FIFO_MAXLEN
+    if (audioFifoCnt + frames >= AUDIO_FIFO_MAXLEN) {
+        //console.log('o', audioFifoCnt)
+        return
+    }
+    for (var i = 0; i < frames; i++) {
+        audioFifo0[tail] = wasmAudioBuf[i * 2]
+        audioFifo1[tail] = wasmAudioBuf[i * 2 + 1]
+        tail = (tail + 1) % AUDIO_FIFO_MAXLEN
+    }
+    audioFifoCnt += frames
+}
